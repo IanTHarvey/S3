86aaa0f1a226cb0d655feabb9b9c4d7aae323dbd
Ian Harvey <Ian.Harvey@ppm.co.uk>
2017-04-18 17:15:04 +0100

    Separated out some defines

---
+++ S3DataModelDefs.h
@@ -0,0 +1,27 @@
+#pragma once
+
+// Maximal configurations
+#define S3_MAX_RXS				6
+#define S3_MAX_TXS				6
+#define S3_MAX_IPS				8
+
+#define S3_N_CHARGERS			4
+
+#define S3_TREE_DEPTH			3
+
+#define SHA1_DIGEST_LEN			20 // 160-bit
+#define SHA1_KEY_LEN			16 // 128-bit
+
+#define S3_MAX_MODEL_ID_LEN		64
+#define S3_MAX_SN_LEN			64
+#define S3_MAX_PN_LEN			64
+#define S3_MAX_SW_VER_LEN		32
+#define S3_MAX_FW_DATE_LEN		16
+#define S3_MAX_TIME_STR_LEN		32		// In ISO 8601 standard format
+#define S3_MAX_INFO_STR_LEN		128
+#define S3_MAX_OS_IMAGE_ID_LEN	64		// Normally ~30
+#define S3_MAX_BUILD_NUM_LEN	64
+#define S3_MAX_USB_PORT_LEN     64
+#define S3_MAX_USB_DRIVER_LEN   64
+
+// ----------------------------------------------------------------------------
