872d632b535d25a3fa864c8aa0f82651e47bb1c4
Ian Harvey <ian.harvey@ppm.co.uk>
2017-11-27 17:14:58 +0000

    New release 1.03.003

--- S3SystemDetails.h
+++ S3SystemDetails.h
@@ -13,8 +13,10 @@
 // 1.02.000 R&S release
 // 1.03.001 R&S Added static IP assignment to application only
 // 1.03.002 As 1.03.001 but new image build
+// 1.03.003 Ignore TxOpt 0xCC:0 triggered by peak detection which is not sorted.
+//			Also, correcting it caused failure to trac certain gain changes.
 
-#define S3_SYS_SW			"1.03r002"
+#define S3_SYS_SW			"1.03r003"
 
 #define S3_SYS_MODEL		"Sentinel 3"
 
