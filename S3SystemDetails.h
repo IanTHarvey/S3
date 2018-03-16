03f82405f11a2f32d01772a820ddb0399c8cbe38
Ian Harvey <ian.harvey@ppm.co.uk>
2018-03-16 14:57:00 +0000

    1.04.000 release

--- S3SystemDetails.h
+++ S3SystemDetails.h
@@ -14,9 +14,12 @@
 // 1.03.001 R&S Added static IP assignment to application only
 // 1.03.002 As 1.03.001 but new image build
 // 1.03.003 Ignore TxOpt 0xCC:0 triggered by peak detection which is not sorted.
-//			Also, correcting it caused failure to trac certain gain changes.
+//			Also, correcting it caused failure to track certain gain changes.
+// 1.03.004 
+// 1.03.005 Rx+ support added, mods and fixes
+// 1.04.000 Release to production
 
-#define S3_SYS_SW			"1.03r004"
+#define S3_SYS_SW			"1.04.000" // Not released
 
 #define S3_SYS_MODEL		"Sentinel 3"
 
