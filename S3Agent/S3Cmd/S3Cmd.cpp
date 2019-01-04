371d7da58e21ac15f86a645703540a818cb7c235
Ian Harvey <ian.harvey@ppm.co.uk>
2019-01-04 08:03:59 +0000

    Added tidy-up return path and handle "W:" return messages

--- S3Agent/S3Cmd/S3Cmd.cpp
+++ S3Agent/S3Cmd/S3Cmd.cpp
@@ -35,11 +35,16 @@ int _tmain(int argc, _TCHAR* argv[])
 	char Cmd[MAX_CMD_LEN];
 	char IPAddr[MAX_IP_ADDR_LEN];
 	char IPPort[MAX_IP_ADDR_LEN];
+	wchar_t Filename[MAX_PATH];
 	size_t nconv;
 	FILE *fid = NULL;
 
+	int ret;
+
+	// Set default
 	strcpy_s(IPPort, MAX_IP_ADDR_LEN, IPV4Port);
 
+	// Parse arguments - jumping out at the first sign of trouble is not ideal
 	if (argc < 3)
 	{
 		printf("Not enough arguments\n");
@@ -53,6 +58,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	{
 		if (!wcscmp(_T("-i"), argv[i]))
 		{
+			// Mandatory IPv4 address
 			if (i < argc - 1)
 			{
 				wcstombs_s(&nconv, IPAddr, MAX_IP_ADDR_LEN, argv[i + 1],
@@ -62,7 +68,8 @@ int _tmain(int argc, _TCHAR* argv[])
 				if (IPv4ToolsRemLeadZ(IPAddr))
 				{
 					fprintf(stderr, "Invalid IP address\n");
-					return 1;
+					ret = 1;
+					goto RETURN;
 				}
 
 				IPAddrGiven = true;
@@ -70,11 +77,13 @@ int _tmain(int argc, _TCHAR* argv[])
 			else
 			{
 				fprintf(stderr, "No IP address specified\n");
-				return 1;
+				ret = 1;
+				goto RETURN;
 			}
 		}
 		else if (!wcscmp(_T("-p"), argv[i]))
 		{
+			// Optional IP port number
 			if (i < argc - 1)
 			{
 				wchar_t *end;
@@ -84,7 +93,8 @@ int _tmain(int argc, _TCHAR* argv[])
 					|| end == argv[i + 1] || p < 0 || p > 65535)
 				{
 					fprintf(stderr, "Invalid IP port number\n");
-					return 1;
+					ret = 1;
+					goto RETURN;
 				}
 
 				wcstombs_s(&nconv, IPPort, MAX_IP_ADDR_LEN, argv[i + 1],
@@ -94,11 +104,13 @@ int _tmain(int argc, _TCHAR* argv[])
 			else
 			{
 				fprintf(stderr, "No port specified\n");
-				return 1;
+				ret = 1;
+				goto RETURN;
 			}
 		}
 		else if (!wcscmp(_T("-o"), argv[i]))
 		{
+			// Optional S3 output file
 			if (i < argc - 1)
 			{
 				errno_t err = _wfopen_s(&fid, argv[i + 1], _T("w"));
@@ -108,31 +120,39 @@ int _tmain(int argc, _TCHAR* argv[])
 				{
 					fprintf(stderr, "Failed to open file: %S (%d)",
 						argv[i + 1], err);
-					return 1;
+					ret = 1;
+					goto RETURN;
 				}
+
+				wcscpy_s(Filename, MAX_PATH, argv[i + 1]);
 			}
 		}
 		else if (argv[i][0] == '-')
 		{
+			// Any known options handled previously
 			fprintf(stderr, "Unknown command line argument: %S", argv[i]);
-			return 1;
+			ret = 1;
+			goto RETURN;
 		}
 	}
 
 	if (!IPAddrGiven)
 	{
 		fprintf(stderr, "No IP address given\n");
-		return 1;
+		ret = 1;
+		goto RETURN;
 	}
 
 	if (lastarg >= argc - 1)
 	{
 		fprintf(stderr, "No remote command\n");
-		return 1;
+		ret = 1;
+		goto RETURN;
 	}
 
 	*Cmd = '\0';
 
+	// Extract S3 command
 	char tmp[MAX_CMD_LEN];
 	for(int i = lastarg + 1;  i < argc; i++)
 	{
@@ -141,16 +161,26 @@ int _tmain(int argc, _TCHAR* argv[])
 		strcat_s(Cmd, MAX_CMD_LEN, tmp);
 	}
 
+	// Try sending and get reply
 	char res[DEFAULT_BUFLEN];
-	int ret = S3SendMessage(res, IPAddr, IPPort, Cmd);
+	ret = S3SendMessage(res, IPAddr, IPPort, Cmd);
 
 	if (ret)
+	{
+		// S3Cmd command failed
 		fprintf(stderr, "Result: Error (%d): %s\n", ret, res);
+	}
 	else
 	{
-		if (!_strnicmp("OK:", res, 3) || !_strnicmp("I:", res, 2))
+		// S3Cmd succeeded, but S3 may have returned an error
+		if (	!_strnicmp("OK:",	res, 3) ||
+				!_strnicmp("I:",	res, 2) ||
+				!_strnicmp("W:",	res, 2))
 			ret = 0;
-		else ret = 2;
+		else if (!_strnicmp("E:",	res, 2))
+			ret = 2;
+		else
+			ret = 1; // Nonsense returned
 
 		printf("Result(%d): %s\n", ret, res);
 
@@ -160,8 +190,12 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 	}
 
+RETURN:	// Clean up
+
 	if (fid)
+	{
 		fclose(fid);
+	}
 
 	return ret;
 }
