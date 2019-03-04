56c56b3acb04d871f4d3349543d1cdd5b58d687e
Ian Harvey <ian.harvey@ppm.co.uk>
2019-03-04 07:49:11 +0000

    Re-instate an old exe if not found in run-time directory NCR1422

--- S3Boot/S3Boot.cpp
+++ S3Boot/S3Boot.cpp
@@ -2,10 +2,6 @@
 //
 // Sentinel 3 installation and update loader.
 //
-// TODO:
-// Use better update exe naming convention (add obfuscation).
-// Check update later than current (incorporate date/version in above).
-// 
 
 #include "windows.h"
 #include "Winbase.h"
@@ -16,23 +12,26 @@
 
 #define S3_MAX_FILENAME_LEN		128
 
-#define	S3_ROOT_DIR		"\\FlashDisk\\S3"
-#define S3_RUN_PATH		"\\Flashdisk\\S3Controller"
-#define S3_RUN_DIR		"S3Controller"
-#define S3_HOME_DIR		"\\Flashdisk\\S3"
-#define S3_IMAGE_FILE	"nk.nb0"
-#define S3_REG_FILE		"Registry.dat"
+#define	S3_ROOT_DIR			"\\FlashDisk\\S3"
+#define S3_RUN_PATH			"\\Flashdisk\\S3Controller"
+#define S3_RUN_DIR			"S3Controller"
+#define S3_HOME_DIR			"\\Flashdisk\\S3"
+#define S3_IMAGE_FILE		"nk.nb0"
+#define S3_REG_FILE			"Registry.dat"
 
+#define S3_PANIC_FILE		_T(S3_HOME_DIR) _T("\\S3Panic.s3p")
+#define S3_VERSION_FILE		_T(S3_HOME_DIR) _T("\\S3Boot.s3v")
 #define S3_UPDATE_FILENAME	"S3CUpdate.upd"
 #define S3_EXE_NAME			"S3Controller.exe"
 #define S3_EXE_BAK_NAME		"S3Controller.bak"
 
-// #define S3_BOOT_VERSION		"1.1"
-// #define S3_BOOT_VERSION		"1.2"	// Detects OS update file. If found, copies
-										// S3Controller.exe from \Windows
-// #define S3_BOOT_VERSION		"1.3"	// Also copies support files from \Windows
-// #define S3_BOOT_VERSION		"1.4"	// Deletes saved registry if present
-#define S3_BOOT_VERSION			"1.5"	// No change
+// #define S3_BOOT_VERSION		"1.1"		// Initial version
+// #define S3_BOOT_VERSION		"1.2"		// Detects OS update file. If found, copies
+											// S3Controller.exe from \Windows
+// #define S3_BOOT_VERSION		"1.3"		// Also copies support files from \Windows
+// #define S3_BOOT_VERSION		"1.4"		// Deletes saved registry if present
+// #define S3_BOOT_VERSION		"1.5"		// No change
+#define S3_BOOT_VERSION			"1.06.000"	// Detect and mitigate missing S3Controller.exe
 
 #define S3_OSDATE_FILENAME	"S3OSDate"
 
@@ -52,14 +51,23 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	char s[] = {"Sentinel 3"};
 	char buf[2 * SHA256::DIGEST_SIZE + 1];
+	FILE	*Fver;
+	
 	sha256(buf, s, 10);
+	
+	// May be useful to report by S3Controller in future
+	err = _wfopen_s(&Fver, S3_VERSION_FILE, _T("w"));
+	if (!err)
+	{
+		fprintf(Fver, "%s", S3_BOOT_VERSION);
+		fclose(Fver);
+	}
+
+	err = fopen_s(&logfid, "\\Flashdisk\\S3Boot.log", "w");
+	fprintf(logfid, "Started S3Boot (v%s).\n", S3_BOOT_VERSION);
 
 	// ---------------------------------------------------------------------------
 	// FIRST USE INSTALLATION
-
-	err = fopen_s(&logfid, "\\Flashdisk\\S3Boot.log", "w");
-
-	fprintf(logfid, "Started S3Boot (v%s).\n", S3_BOOT_VERSION);
 
 	fileAtt = GetFileAttributes(_T(S3_RUN_PATH));
 
@@ -80,7 +88,8 @@ int _tmain(int argc, _TCHAR* argv[])
 
 		if (berr == 0)
 		{
-			fprintf(logfid, "Failed to create S3Controller directory\n");
+			fprintf(logfid, "Failed to create S3Controller directory: %d\n",
+				GetLastError());
 			ret_err = 1;
 		}
 
@@ -109,7 +118,8 @@ int _tmain(int argc, _TCHAR* argv[])
 
 		if (berr == 0)
 		{
-			fprintf(logfid, "Failed to create S3 directory\n");
+			fprintf(logfid, "Failed to create S3 directory: %d\n",
+				GetLastError());
 			ret_err = 3;
 		}
 		else
@@ -267,7 +277,75 @@ int _tmain(int argc, _TCHAR* argv[])
 	else
 		fprintf(logfid, "No application update file found\n");
 
-	// Signal completed for dependents (S3Controller.exe)
+	// Final test; if no /Flashdisk/S3Controller.exe, S3 is effectively bricked
+	// as no Ethernet services started if already locked. So look to re-instate
+	// from back-up file or executable from \Windows image.
+
+	swprintf_s(src, S3_MAX_FILENAME_LEN, _T("%S\\%S"), S3_RUN_PATH, S3_EXE_NAME);
+	
+	fileAtt = GetFileAttributes(src);
+	if (fileAtt == INVALID_FILE_ATTRIBUTES)
+	{
+		fprintf(logfid, "PANIC: No application file found, finding alternate\n");
+
+		// Create a panic notification file so that S3Controller can do some
+		// notification if required.
+		FILE *FPanic;
+		err = _wfopen_s(&FPanic, S3_PANIC_FILE, _T("w"));
+
+		if (err)
+		{
+			fprintf(logfid, "Failed to create panic notification file: %d\n",
+															GetLastError());
+		}
+		else
+		{
+			fprintf(logfid, "Created panic notification file\n");
+			fclose(FPanic);
+		}
+
+		// Look for BAK file
+		swprintf_s(src, S3_MAX_FILENAME_LEN,	_T("%S\\%S"), S3_RUN_PATH, S3_EXE_BAK_NAME);
+		swprintf_s(dest, S3_MAX_FILENAME_LEN,	_T("%S\\%S"), S3_RUN_PATH, S3_EXE_NAME);
+
+		if (CopyFile(src, dest, false) == false)
+		{
+			DWORD gle = GetLastError();
+
+			if (GetLastError() == ERROR_FILE_NOT_FOUND)
+				fprintf(logfid, "No application back-up file found\n");
+			else
+				fprintf(logfid, "Copy of application back-up file failed: %d\n", gle);
+
+			// Look for file in Windows image
+			if (S3BootCopy2Flash(_T(S3_EXE_NAME), _T(S3_RUN_DIR)))
+			{
+				fprintf(logfid, "Failed to copy S3Controller from Windows image to run-time directory\n");
+			}
+			else
+			{
+				fprintf(logfid, "Copied S3Controller from Windows image to run-time directory\n");
+			}
+		}
+		else
+			fprintf(logfid, "Application back-up file copied\n");
+
+		fprintf(logfid, "PANIC: Measures finished\n");
+	}
+	else
+	{
+		fprintf(logfid, "Application file present, no panic\n");
+
+		if (DeleteFile(S3_PANIC_FILE) == false)
+		{
+			if (GetLastError() != ERROR_FILE_NOT_FOUND)
+				fprintf(logfid, "Failed to delete panic file\n");
+		}
+		else
+			fprintf(logfid, "Panic file deleted\n");
+	}
+
+	// Signal completed for dependants (S3Controller.exe)
 	// See /HKLM/init
 
 #ifdef _WIN32_WCE
